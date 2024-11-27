fd26afd21c26534ef44e458d15527d29fecd9f9b
jsaydam <jsaydam@interac.ca>
2024-11-27 17:23:14 -0500

    Create main.tf

---
+++ iac_config/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_version = ">= 1.3.0"
+  required_providers {
+    aws    = {
+      source  = "hashicorp/aws"
+      version = "5.77.0"
+    }
+}
+
+local "vpc_name" {
+   value  =  vpc-${var.app_environment}
+}
+
+module "team10_network" {
+  source      = "mtlmtfe01.mgmt.interac.ca/DevSecOpsHackathon/team10_network/aws"
+  version     = "1.0.0"
+  cidr_block  = var.cidr_block
+  env         = var.app_environment
+  region      = var.region
+  vpc_enabled = true
+  vpc_name    = local.vpc_name
+  azs         = var.azs
+}
