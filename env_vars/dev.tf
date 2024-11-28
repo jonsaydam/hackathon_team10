6085770faf26a85c9fcc574d8d247a3dde4a2a32
Brijesh Katherla <bkatherla@interac.ca>
2024-11-28 14:00:23 -0500

    Updating the variables to pass for the modules

--- env_vars/dev.tf
+++ env_vars/dev.tf
@@ -1,8 +1,8 @@
-variable "app_environment" {
+variable "env" {
 default = "dev" 
 }
 
-variable "vpc_cidr" {
+variable "cidr_block" {
   default = "10.0.0.0/24"
 }
 
@@ -18,7 +18,82 @@ variable "azs" {
   default  = ["us-east-1a", "us-east-1b"]
 }
 
-locals {
-   vpc_name  =  "vpc-${var.app_environment}"
+variable "aurora_enabled" {
+  default  = true
 }
 
+variable "cluster_identifier" {
+  default  = "${env}-aurora-cluster"
+}
+
+variable "engine" {
+  default  = "aurora-postgresql"
+}
+
+variable "engine_version" {
+  default  = "15.4"
+}
+
+variable "database_name" {
+  default  = "${env}-database"
+}
+
+variable "master_username" {
+  default  = "admin"
+}
+
+variable "master_password" {
+  default  = "pass"
+}
+
+variable "backup_retention_period" {
+  default  = 7
+}
+
+variable "storage_encrypted" {
+  default  = true
+}
+
+variable "kms_key_id" {
+  default  = "alias/${env}-aurora-kms"
+}
+
+variable "deletion_protection" {
+  default  = true
+}
+
+variable "preferred_backup_window" {
+  default  = "07:00-09:00"
+}
+
+variable "preferred_maintenance_window" {
+  default  = "Sun:23:00-Mon:01:00"
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  default  = ["postgresql", "audit"]
+}
+
+variable "instance_count" {
+  default  = 2
+}
+
+variable "instance_class" {
+  default  = "db.t3.medium"
+}
+
+variable "publicly_accessible" {
+  default  = false
+}
+
+variable "apply_immediately" {
+  default  = true
+}
+
+variable "auto_minor_version_upgrade" {
+  default  = true
+}
+           
+locals {
+   vpc_name  =  "vpc-${var.env}"
+}
--- env_vars/prod.tf
+++ env_vars/prod.tf
@@ -1,4 +1,4 @@
-variable "app_environment" {
+variable "env" {
 default = "prod" 
 }
 
@@ -18,6 +18,82 @@ variable "azs" {
   default  = ["us-east-1a", "us-east-1b"]
 }
 
+variable "aurora_enabled" {
+  default  = true
+}
+
+variable "cluster_identifier" {
+  default  = "${env}-aurora-cluster"
+}
+
+variable "engine" {
+  default  = "aurora-postgresql"
+}
+
+variable "engine_version" {
+  default  = "15.4"
+}
+
+variable "database_name" {
+  default  = "${env}-database"
+}
+
+variable "master_username" {
+  default  = "admin"
+}
+
+variable "master_password" {
+  default  = "pass"
+}
+
+variable "backup_retention_period" {
+  default  = 7
+}
+
+variable "storage_encrypted" {
+  default  = true
+}
+
+variable "kms_key_id" {
+  default  = "alias/${env}-aurora-kms"
+}
+
+variable "deletion_protection" {
+  default  = true
+}
+
+variable "preferred_backup_window" {
+  default  = "07:00-09:00"
+}
+
+variable "preferred_maintenance_window" {
+  default  = "Sun:23:00-Mon:01:00"
+}
+
+variable "enabled_cloudwatch_logs_exports" {
+  default  = ["postgresql", "audit"]
+}
+
+variable "instance_count" {
+  default  = 2
+}
+
+variable "instance_class" {
+  default  = "db.t3.medium"
+}
+
+variable "publicly_accessible" {
+  default  = false
+}
+
+variable "apply_immediately" {
+  default  = true
+}
+
+variable "auto_minor_version_upgrade" {
+  default  = true
+}
+
 locals {
-   vpc_name  =  "vpc-${var.app_environment}"
+   vpc_name  =  "vpc-${var.env}"
 }
