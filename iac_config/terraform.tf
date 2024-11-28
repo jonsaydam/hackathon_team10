8d3674e5333e82cedd615f1a14c5b144f2c454f6
Brijesh Katherla <bkatherla@interac.ca>
2024-11-28 15:18:55 -0500

    syntax correction

--- iac_config/terraform.tf
+++ iac_config/terraform.tf
@@ -3,7 +3,7 @@ terraform {
     postgresql = {
       source = "cyrilgdn/postgresql"
       version = "1.24.0"
-    },
+    }
     aws    = {
       source  = "hashicorp/aws"
       version = ">= 3.0.0"
