a9ba393606364b0b59262cd4bc028b44b7955364
jsaydam <jsaydam@interac.ca>
2024-11-28 14:52:33 -0500

    Update terraform.tf

--- iac_config/terraform.tf
+++ iac_config/terraform.tf
@@ -3,6 +3,14 @@ terraform {
     postgresql = {
       source = "cyrilgdn/postgresql"
       version = "1.24.0"
+    },
+    aws    = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+    vault  = {
+      source  = "hashicorp/vault"
+      version = "4.5.0"
     }
   }
 }
