664b8e10cc8797c20002134125c168ec9cdf2b5e
jonsaydam <41655125+jonsaydam@users.noreply.github.com>
2024-11-29 14:38:51 -0500

    Update test_data_integrity.sh

--- test/test_data_integrity.sh
+++ test/test_data_integrity.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-DB_HOST=`cat terraform_output.json | sed 's/"//g'`
-
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT * FROM payee;"
+psql -h $DB_HOST -U ${{ secrets.DB_USER }} -d ${{ secrets.DB_NAME }} -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
+psql -h $DB_HOST -U ${{ secrets.DB_USER }} -d ${{ secrets.DB_NAME }} -c "SELECT * FROM payee;"
