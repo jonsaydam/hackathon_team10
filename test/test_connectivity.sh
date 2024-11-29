7b4045441ed2b0c313c6efd8166f943ad30afdd9
Jon <jsaydam@interac.ca>
2024-11-29 08:41:09 -0500

    host

--- test/test_connectivity.sh
+++ test/test_connectivity.sh
@@ -5,4 +5,4 @@ DB_HOST=`cat terraform_output.json`
 DB_USER=app_admin
 DB_NAME=devdatabase
 
-psql -h $DB_HOST -U $DB_USER -c "\conninfo"
+psql -h dev-aurora-cluster.cluster-cj8e6euumn25.us-east-1.rds.amazonaws.com -U $DB_USER -c "\conninfo"
