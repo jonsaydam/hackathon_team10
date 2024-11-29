65dae3db7bcb4ef5ea9ff9565142882a1516eaf0
jonsaydam <41655125+jonsaydam@users.noreply.github.com>
2024-11-29 14:37:24 -0500

    Update init_schema.sh

--- schemas/init_schema.sh
+++ schemas/init_schema.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 set -e
 
-DB_HOST=`cat terraform_output.json | sed 's/"//g'`
-DB_USER=app_admin
-
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ./create_schema.sql
+psql -h $DB_HOST -U ${{ secrets.DB_USER }} -d ${{ secrets.DB_NAME }} -f ./create_schema.sql
