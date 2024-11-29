fa4928d41e800e9c5501bacebf978c9a3b5317d6
Jon <jsaydam@interac.ca>
2024-11-29 08:40:06 -0500

    host

--- schemas/init_schema.sh
+++ schemas/init_schema.sh
@@ -4,4 +4,4 @@ set -e
 DB_HOST=`cat terraform_output.json`
 DB_USER=app_admin
 
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ./create_schema.sql
+psql -h dev-aurora-cluster.cluster-cj8e6euumn25.us-east-1.rds.amazonaws.com -U $DB_USER -d $DB_NAME -f ./create_schema.sql
--- test/test_data_integrity.sh
+++ test/test_data_integrity.sh
@@ -5,5 +5,5 @@ DB_HOST=`cat terraform_output.json`
 DB_USER=app_admin
 DB_NAME=devdatabase
 
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
-psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT * FROM payee;"
+psql -h dev-aurora-cluster.cluster-cj8e6euumn25.us-east-1.rds.amazonaws.com -U $DB_USER -d $DB_NAME -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
+psql -h dev-aurora-cluster.cluster-cj8e6euumn25.us-east-1.rds.amazonaws.com -U $DB_USER -d $DB_NAME -c "SELECT * FROM payee;"
