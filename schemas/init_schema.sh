7dc4eee7419110cc71ab4c643aa2f2edb3f46443
jonsaydam <41655125+jonsaydam@users.noreply.github.com>
2024-11-29 14:14:35 -0500

    Update init_schema.sh

--- schemas/init_schema.sh
+++ schemas/init_schema.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-DB_HOST=`cat terraform_output.json`
+DB_HOST=`cat terraform_output.json | sed 's/"//g'`
 DB_USER=app_admin
 
-psql -h dev-aurora-cluster.cluster-cj8e6euumn25.us-east-1.rds.amazonaws.com -U $DB_USER -d $DB_NAME -f ./create_schema.sql
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -f ./create_schema.sql
