49f9cd8b5c0757a2c0c8065c0084372ee8766b35
jonsaydam <41655125+jonsaydam@users.noreply.github.com>
2024-11-29 15:27:03 -0500

    Update test_data_integrity.sh

--- test/test_data_integrity.sh
+++ test/test_data_integrity.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 
-psql -h $DB_HOST -U ${{ secrets.DB_USER }} -d ${{ secrets.DB_NAME }} -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
-psql -h $DB_HOST -U ${{ secrets.DB_USER }} -d ${{ secrets.DB_NAME }} -c "SELECT * FROM payee;"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "INSERT INTO payee (email, name, phone_number) VALUES ('testuser@example.com', 'Test User', '1234567890');"
+psql -h $DB_HOST -U $DB_USER -d $DB_NAME -c "SELECT * FROM payee;"
